057c91158fb7b3d367e3e58c216d0d9d65e7f18f
tavonh1 <30598747+tavonh1@users.noreply.github.com>
2018-01-22 11:56:04 -0600

    Update index.php
    Added mock connection to database.

--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
 <?php
 echo "The Giganto Asset Management System is coming soon!!!!";
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
 ?>
