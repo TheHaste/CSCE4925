df7c8e1a3f94194ca28aa1e3cd7d5e2fcde12c21
tavonh1 <30598747+tavonh1@users.noreply.github.com>
2018-01-22 11:59:45 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,10 +3,14 @@ echo "The Giganto Asset Management System is coming soon!!!!";
 
 // Create database
 $sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
+
+    if ($conn->query($sql) === TRUE) 
+    {
+        echo "Database created successfully";
+    } 
+    else 
+    {
+        echo "Error creating database: " . $conn->error;
+    }
 
 ?>
