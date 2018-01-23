769ee4d78af7219564a7d1ea8c9b9f6949fe66b6
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-01-23 12:23:55 -0600

    Testing for php

--- web/index.php
+++ web/index.php
@@ -4,13 +4,13 @@ echo "The Giganto Asset Management System is coming soon!!!!";
 // Create database
 $sql = "CREATE DATABASE myDB";
 
-    if ($conn->query($sql) === TRUE) 
+/*    if ($conn->query($sql) === TRUE) 
     {
         echo "Database created successfully";
     } 
     else 
     {
-        echo "Error creating database: " . $conn->error;
-    }
+*/        echo "Error creating database: ";// . $conn->error;
+ //   }
 
 ?>
