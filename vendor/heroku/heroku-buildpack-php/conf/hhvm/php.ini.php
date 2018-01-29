0710e5d2a63fa75fdf5a21c8d80dba9faf0f8018
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-01-29 11:59:37 -0600

    test

--- vendor/heroku/heroku-buildpack-php/conf/hhvm/php.ini.php
+++ vendor/heroku/heroku-buildpack-php/conf/hhvm/php.ini.php
@@ -15,3 +15,4 @@ hhvm.server.type = fastcgi
 hhvm.server.file_socket = /tmp/heroku.fcgi.<?=getenv('PORT')?>.sock
 hhvm.server.thread_count = <?=getenv('WEB_CONCURRENCY')?:8?>
 
+curl.cainfo = /app/etc/ssl/certs/cacert.pem
