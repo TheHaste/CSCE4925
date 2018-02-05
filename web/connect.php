6c5c22ac6c136356e20cd4f599ff3f7e22d72025
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-05 13:08:37 -0800

    Update connect.php

--- web/connect.php
+++ web/connect.php
@@ -1,16 +1,18 @@
 <?php
 
-$dbconn = pg_connect("dbname=ec2-54-235-244-185.compute-1.amazonaws.com") or die("Could not connect");
-  $stat = pg_connection_status($dbconn);
-  if ($stat === PGSQL_CONNECTION_OK) {
-      echo 'Connection status ok';
-  } else {
-      echo 'Connection status bad';
-  }    
+   $con = "dbname=d8u7tvrq18kkt host=ec2-54-235-244-185.compute-1.amazonaws.com port=5432 user=wseudmwqkwlvhe password=68662358186783d87ddb7c2f1676eff1c5eb41678bb8cc8b9b16ea1e420abc2b sslmode=require";
+
+
+   if (!$con) 
+   {
+     echo "Database connection failed.";
+   }
+   else 
+   {
+     echo "Database connection success.";
+   }
 
 ?>
-
-
 
 
 
