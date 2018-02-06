e1e279854aef889bc9013afb97efef90e512e5c7
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-02-05 20:09:22 -0600

    test

--- web/login/index.php
+++ web/login/index.php
@@ -1,16 +1,16 @@
 <?php
-require('/app/web/connect.php');
+//require('/app/web/connect.php');
 
 session_start(); //start user session to send data between pages
 
 if($_POST['admin'] == 'Admin Button'){ //if admin button is pressed
 	$_SESSION["userType"] = 'admin';
-	header('Location: /home/index.php');
+	header('Location: /app/web/home/');
 }
 
 if($_POST['user'] == 'User Button'){ //if user button is pressed
 	$_SESSION["userType"] = 'user';
-	header('Location: /home/');
+	header('Location: /app/web/home/');
 }
 
 ?>
