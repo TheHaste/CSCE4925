8b0b94be1561821d303f230fd8f338460e7e0a12
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-05 17:16:47 -0800

    Update connect.php

--- web/connect.php
+++ web/connect.php
@@ -1,17 +1,15 @@
 <?php
+$servername = "ec2-54-235-244-185.compute-1.amazonaws.com";
+$username = "wseudmwqkwlvhe";
+$password = "68662358186783d87ddb7c2f1676eff1c5eb41678bb8cc8b9b16ea1e420abc2b";
+$dbname = "d8u7tvrq18kkt";
 
-   $con = "dbname=d8u7tvrq18kkt host=ec2-54-235-244-185.compute-1.amazonaws.com port=5432 user=wseudmwqkwlvhe password=68662358186783d87ddb7c2f1676eff1c5eb41678bb8cc8b9b16ea1e420abc2b sslmode=require";
-
-
-   if (!$con) 
-   {
-     echo "Database connection failed.";
-   }
-   else 
-   {
-     echo "Database connection success.";
-   }
-
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 ?>
 
 
