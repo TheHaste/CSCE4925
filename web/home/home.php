908bde45b1b2b33c79f7e53d0544f0baa6d8a32e
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-09 12:47:03 -0800

    Update home.php

--- web/home/home.php
+++ web/home/home.php
@@ -6,7 +6,7 @@ session_start(); //start user session to send data between pages
 if($_SESSION["userType"] == 'admin'){ //if admin button is pressed
 	echo "Hello I'm an admin!!!";
 }
-else if($_SESSION["userType1"] == 'user'){
+else if($_SESSION["userType"] == 'user'){
 	echo "Hello I am a standard user!!";
 }
 
