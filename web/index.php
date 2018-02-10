24f90d948e834d8a6b81eb43ce379c182e5e9e41
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-10 15:36:44 -0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -17,36 +17,40 @@ if($_POST['user'] == 'User Button'){ //if user button is pressed
 
 
 
-<!DOCTYPE html>
 <html>
+
+
+
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login page</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/Login-Center.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
+	 <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
     <div class="container">
-        <div class="row row-login" style="padding-bottom:-2px;padding-left:-5px;padding-top:100px;">
-            <div class="col-10 col-sm-6 col-md-4 offset-1 offset-sm-3 offset-md-4 offset-lg-5">
-                <h1 class="text-center">Meridian Solutions</h1>
-                <div class="card">
-                    <div class="card-body">
-                        <h3 class="text-danger">Login </h3>
-                        <form>
-                            <div class="form-group"><label>Username </label><input class="form-control" type="text"></div>
-                            <div class="form-group"><label>Password </label><input class="form-control" type="password"></div><button class="btn btn-success btn-block" type="submit">LOGIN </button></form>
-                    </div>
+        <div class="row row-login" style="margin-left:-25px;">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                <h1 class="text-center">Giganto Asset Management</h1>
+                <div class="well">
+                    <form method= "POST">
+						<input class="btn btn-success btn-block" type="submit" value="Admin Button" name='admin' style="margin-bottom:21px;margin-top:19px;"></input>
+						<div style="height:5px"></div>
+					    <input class="btn btn-success btn-block" type="submit" value="User Button" name='user' style="margin-bottom:21px;margin-top:19px;"></input>
+					</form>
                 </div>
             </div>
         </div>
     </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/js/jquery-1.12.4.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+    <script id="bs-live-reload" data-sseport="50697" data-lastchange="1516477633766" src="/js/livereload.js"></script>
 </body>
 
 </html>
