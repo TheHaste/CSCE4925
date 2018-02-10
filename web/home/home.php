70ef9ffed01e63b88682c0d6179ad795f3a37344
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-09 17:42:45 -0800

    Update home.php

--- web/home/home.php
+++ web/home/home.php
@@ -24,7 +24,7 @@ else if($_SESSION["userType"] == 'user'){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="ALL_BOOTSTRAP FILES/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,400italic">
     <link rel="stylesheet" href="assets/css/user.css">
     <link rel="stylesheet" href="assets/css/Data-Table.css">
