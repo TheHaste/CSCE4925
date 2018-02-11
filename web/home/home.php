f907e9c6ce583c8832a51dcc651113585e3c9ae8
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-10 17:28:35 -0800

    Update home.php

--- web/home/home.php
+++ web/home/home.php
@@ -4,10 +4,10 @@ require('/app/web/connect.php');
 session_start(); //start user session to send data between pages
 
 if($_SESSION["userType"] == 'admin'){ //if admin button is pressed
-	echo "Hello I'm an admin!!!";
+	//echo "Hello I'm an admin!!!";
 }
 else if($_SESSION["userType"] == 'user'){
-	echo "Hello I am a standard user!!";
+	//echo "Hello I am a standard user!!";
 }
 
 ?>
