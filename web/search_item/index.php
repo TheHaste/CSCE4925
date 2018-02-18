c69817b3e753339e27cdcf3fa2532ada1a3008fb
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-17 18:29:21 -0800

    Update index.php

--- web/search_item/index.php
+++ web/search_item/index.php
@@ -79,7 +79,7 @@
         </form>
     </div>
     <div class="col-md-12 col-md-offset-0" style="width:250px;padding-bottom:486px;padding-right:-1px;margin-top:-160px;">
-        <div class="search-container"></div>
+      <!--  <div class="search-container"></div>-->
         <h1 style="font-size:13px;color:rgb(255,1,1);padding-bottom:36px;margin-top:62px;"><form class="form-inline">
   <div class="form-group">
     
