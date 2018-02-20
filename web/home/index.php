1b58a7a4652c8a7ed2b05ba0897e499cf32a191d
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-02-19 22:20:24 -0600

    Home Index

--- web/home/index.php
+++ web/home/index.php
@@ -26,35 +26,33 @@ else if($_SESSION["userType"] == 'user'){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&amp;subset=cyrillic,latin-ext">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,900i&amp;subset=cyrillic,latin-ext">
     <link rel="stylesheet" href="assets/css/Article-Dual-Column.css">
     <link rel="stylesheet" href="assets/css/Features-Boxed.css">
     <link rel="stylesheet" href="assets/css/Google-Style-Text-Input.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+    <link rel="stylesheet" href="assets/css/MUSA_panel-table.css">
+    <link rel="stylesheet" href="assets/css/MUSA_panel-table1.css">
     <link rel="stylesheet" href="assets/css/Navigation-with-Search1.css">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/thumbnails1.css">
 	
-	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"></link>
-	<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	
+	<link rel="stylesheet" type="text/css" href="/DataTables/datatables.min.css"/>
+	<script type="text/javascript" src="/DataTables/datatables.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+	<script type="text/javascript" src="DataTables/datatables.min.js"></script>
 
 	
 </head>
 
 <!--Nav bar settings-->
 <body style="padding-left:-1px;">
-<nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header"><a class="navbar-brand" href="#"> </a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
-            <div
-                class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav navbar-right"></ul>
-        </div>
-        </div>
-    </nav>
     <div>
 	<!--Nav bar settings-->
         <nav class="navbar navbar-default navigation-clean" style="background-color:rgb(72,143,174);min-width:0px;max-width:100%;margin-right:0px;margin-top:-51px;">
@@ -77,7 +75,7 @@ else if($_SESSION["userType"] == 'user'){
 
 
 <div class="container">
-<!--  <div class="row" width="1000px"> -->
+    <div class="row">
    
     
         <div class="col-md-10 col-md-offset-1">
@@ -92,7 +90,7 @@ else if($_SESSION["userType"] == 'user'){
               </div>
 			  
               <div class="panel-body">
-                <table id="assets" class="display" width="100%">
+                <table id="assets" class="display nowrap" cellspacing="0" width="100%">
                   <thead>
                     <tr>
                         <th>Name</th>
@@ -145,18 +143,33 @@ else if($_SESSION["userType"] == 'user'){
 			</div>
 		  
 		</div>
-<!--	</div> -->
+	</div>
         <div style="height:10px;"></div>
 
 </div>
     	
 
     </div>
-
+    <div></div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-1.12.4.js"</script>
+	<script type="text/javascript" src="/DataTable/js/jquery.dataTables.min.js"</script>
+	<script type="text/javascript" src="/DataTable/js/dataTables.bootstrap.min.js"</script>
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.css"> 
+<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
+    <script src="assets/js/bs-animation.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+	
+	<script type="text/javascript"> $(document).ready(function() {
+			$(".table").DataTable();
+		});
+		</script>
 	
     
 </body>
 
+<
 	<script type="text/javascript">$(document).ready(function() {
     $('#assets').DataTable();
 } );</script>
