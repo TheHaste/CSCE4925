7c71becb7214f05ea957ae559529747e9d424a75
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-20 17:58:06 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>search_item page</title>
+    <title>edit_table page</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,900i&amp;subset=cyrillic,latin-ext">
