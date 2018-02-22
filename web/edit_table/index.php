ff8f32c1ae2182647deb45ce6f2dc614217ff37a
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-22 15:00:46 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -2,7 +2,29 @@
 require('/app/web/connect.php');
 session_start(); //start user session to send data between pages
 
-var editor;
+		 
+			   $(function () {
+ $("td").dblclick(function () {
+    var OriginalContent = $(this).text();
+
+    $(this).addClass("cellEditing");
+    $(this).html("<input type="text" value="&quot; + OriginalContent + &quot;" />");
+    $(this).children().first().focus();
+
+    $(this).children().first().keypress(function (e) {
+        if (e.which == 13) {
+            var newContent = $(this).val();
+            $(this).parent().text(newContent);
+            $(this).parent().removeClass("cellEditing");
+        }
+    });
+
+ $(this).children().first().blur(function(){
+    $(this).parent().text(OriginalContent);
+    $(this).parent().removeClass("cellEditing");
+ });
+ });
+});
 
 //search page
 ?>
@@ -136,29 +158,7 @@ var editor;
 							'; 
 						}
 					?>
-					 
-			   $(function () {
- $("td").dblclick(function () {
-    var OriginalContent = $(this).text();
-
-    $(this).addClass("cellEditing");
-    $(this).html("<input type="text" value="&quot; + OriginalContent + &quot;" />");
-    $(this).children().first().focus();
-
-    $(this).children().first().keypress(function (e) {
-        if (e.which == 13) {
-            var newContent = $(this).val();
-            $(this).parent().text(newContent);
-            $(this).parent().removeClass("cellEditing");
-        }
-    });
-
- $(this).children().first().blur(function(){
-    $(this).parent().text(OriginalContent);
-    $(this).parent().removeClass("cellEditing");
- });
- });
-});
+			
                    </tbody>
 			</table>
 			</div>
