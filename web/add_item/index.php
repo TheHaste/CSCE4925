339f231a3b3ede2e3960f1134431f5f18933f00c
tavonh1 <30598747+tavonh1@users.noreply.github.com>
2018-02-22 01:02:23 -0600

    Update index.php

--- web/add_item/index.php
+++ web/add_item/index.php
@@ -9,7 +9,8 @@
 
 //
 //	if(isset($_POST['Save']))
-//	{
+	if(isset($_POST) & !empty($_POST)
+	{
 		//retrieve form data by using the element's name attributes value as key
 		$item = $_POST['Item Name'];
 		$serial = $_POST['Serial #'];
@@ -33,7 +34,7 @@
 		$rs = pg_query($conn, $query); //run query
 		pg_close($conn);
 */
-//	}
+	}
 ?>	
 
 <html>
