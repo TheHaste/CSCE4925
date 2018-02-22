5c60fd188ebe3a0c96d48d4eb3f048ea0110599e
tavonh1 <30598747+tavonh1@users.noreply.github.com>
2018-02-21 22:41:41 -0600

    Update index.php

--- web/add_item/index.php
+++ web/add_item/index.php
@@ -8,8 +8,8 @@
 	session_start(); 
 
 //
-	//if(isset($_POST['Save']))
-	//{
+	if(isset($_POST['Save']))
+	{
 		//retrieve form data by using the element's name attributes value as key
 		$item = $_POST['Item Name'];
 		$serial = $_POST['Serial #'];
@@ -19,9 +19,9 @@
 		$status = $_POST['Status'];
 	 	$cost = $_POST['Cost'];
 		
-		$query = "INSERT INTO 'assets' VALUES ('$item', '$serial', '$location', '$model', '$brand', '$status', '$cost');";
+		$query = "INSERT INTO 'assets' (id, serial_number, location, model, brand, status, cost) VALUES ('$item', '$serial', '$location', '$model', '$brand', '$status', '$cost');";
 		$rs = pg_query($conn, $query); //run query
-	//}
+	}
 ?>	
 
 <html>
