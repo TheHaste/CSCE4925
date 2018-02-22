e22d699800e82daefc8c108a73d606d32ac4360b
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-22 14:04:28 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -2,58 +2,7 @@
 require('/app/web/connect.php');
 session_start(); //start user session to send data between pages
 
-var editor;
 
-// New record
-    $('a.editor_create').on('click', function (e) {
-        e.preventDefault();
- 
-        editor.create( {
-            title: 'Create new record',
-            buttons: 'Add'
-        } );
-    } );
- 
-    // Edit record
-    $('#example').on('click', 'a.editor_edit', function (e) {
-        e.preventDefault();
- 
-        editor.edit( $(this).closest('tr'), {
-            title: 'Edit record',
-            buttons: 'Update'
-        } );
-    } );
- 
-    // Delete a record
-    $('#example').on('click', 'a.editor_remove', function (e) {
-        e.preventDefault();
- 
-        editor.remove( $(this).closest('tr'), {
-            title: 'Delete record',
-            message: 'Are you sure you wish to remove this record?',
-            buttons: 'Delete'
-        } );
-    } );
- $('#example').DataTable( {
-        ajax: "../edit_table",
-        columns: [
-            { data: null, render: function ( data, type, row ) {
-                // Combine the first and last names into a single table field
-                //return data.first_name+' '+data.last_name;
-            } },
-            { data: "position" },
-            { data: "office" },
-            { data: "extn" },
-            { data: "start_date" },
-            { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) },
-            {
-                data: null,
-                className: "center",
-                defaultContent: '<a href="" class="editor_edit">Edit</a> / <a href="" class="editor_remove">Delete</a>'
-            }
-        ]
-    } );
-} );
 
 //search page
 ?>
