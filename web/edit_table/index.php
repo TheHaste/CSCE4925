2b62590904dce628468443bdfb331c3f2c9248a8
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-22 14:59:33 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -40,6 +40,7 @@ var editor;
 	} );</script>
 
 
+
 </head>
 
 
@@ -136,6 +137,28 @@ var editor;
 						}
 					?>
 					 
+			   $(function () {
+ $("td").dblclick(function () {
+    var OriginalContent = $(this).text();
+
+    $(this).addClass("cellEditing");
+    $(this).html("<input type="text" value="&quot; + OriginalContent + &quot;" />");
+    $(this).children().first().focus();
+
+    $(this).children().first().keypress(function (e) {
+        if (e.which == 13) {
+            var newContent = $(this).val();
+            $(this).parent().text(newContent);
+            $(this).parent().removeClass("cellEditing");
+        }
+    });
+
+ $(this).children().first().blur(function(){
+    $(this).parent().text(OriginalContent);
+    $(this).parent().removeClass("cellEditing");
+ });
+ });
+});
                    </tbody>
 			</table>
 			</div>
