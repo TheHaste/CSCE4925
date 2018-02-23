b3b0dac62f56f317950d5746d89fc125c4ca7e6f
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-22 20:02:04 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -34,36 +34,16 @@ session_start(); //start user session to send data between pages
 	<!-- DataTable Javascript Implementation -->
 	<script type="text/javascript">
 	$(document).ready(function() {
-		$('#assets').DataTable(
-		 {
-			dom: 'Bfrtip',
-			lengthMenu: [
-				[ 10, 25, 50, -1 ],
-				[ '10 rows', '25 rows', '50 rows', 'Show all' ]
-			],
-			buttons: [
-				'pageLength',
-				{extend: 'pdf',
-					text: 'Export to PDF',
-					filename: 'Meridian Inventory',
-					exportOptions: {
-						modifier: {
-							page: 'current'
-						}
-					}
-				},
-				{extend: 'excel',
-					text: 'Export to Excel',
-					filename: 'Meridian Inventory',
-					exportOptions: {
-						modifier: {
-							page: 'current'
-						}
-					}
-				}
-			]
-		});	
-	} );
+		$('#assets').DataTable( {
+    buttons: [
+        { extend: 'create', editor: myEditor },
+        { extend: 'edit',   editor: myEditor },
+        { extend: 'remove', editor: myEditor }
+    ]
+		}
+} );
+	</script>
+			
 	
 </script>
 
