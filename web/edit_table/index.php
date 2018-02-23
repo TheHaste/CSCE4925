0d86d2c5c6eeb4258a04e3c98eaf665901382f04
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-22 20:10:07 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -30,9 +30,11 @@ session_start(); //start user session to send data between pages
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script> 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-
-	<!-- DataTable Javascript Implementation -->
-	<script type="text/javascript">
+<script src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
+<link rel="stylesheet" href="https://editor.datatables.net/extensions/Editor/css/editor.dataTables.min.css"></script>
+<script src="https://cdn.datatables.net/select/1.2.5/css/select.dataTables.min.css"></script>
+						   
+						   
 	$(document).ready(function() {
 		$('#assets').DataTable( {
     buttons: [
@@ -43,9 +45,11 @@ session_start(); //start user session to send data between pages
 		}
 } );
 	</script>
+
+
 			
 	
-</script>
+
 
 </head>
 
