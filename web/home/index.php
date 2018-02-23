a746d8b18ca44385053c578aef74b8a4c1a7e96e
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-02-22 18:41:57 -0600

    Home Index

--- web/home/index.php
+++ web/home/index.php
@@ -40,6 +40,7 @@ else if($_SESSION["userType"] == 'user'){
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script> 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
 	<!-- DataTable Javascript Implementation -->
 	<script type="text/javascript">
