ec3753fd8d34247b25ec7b83a37d33a534b490a7
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-22 20:23:10 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -26,37 +26,24 @@ session_start(); //start user session to send data between pages
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.bootstrap.min.js"></script>	
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
-	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script> 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
-<link rel="stylesheet" href="https://editor.datatables.net/extensions/Editor/css/editor.dataTables.min.css"></script>
-<script src="https://cdn.datatables.net/select/1.2.5/css/select.dataTables.min.css"></script>
-						   
-	<script type="text/javascript">	
-		var editor;
-	$(document).ready(function() {
-		
-		$('#assets').DataTable( {
-    buttons: [
-        { extend: 'create', editor: myEditor },
-        { extend: 'edit',   editor: myEditor },
-        { extend: 'remove', editor: myEditor }
-    ]
-		}
-} );
-	</script>
-
-
-			
+	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+	
+	<script src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
+	<link rel= "stylesheet" href="https://editor.datatables.net/extensions/Editor/css/editor.dataTables.min.css"></link>
 	
 
-
+	<!-- DataTable Javascript Implementation -->
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$('#example').DataTable();	
+	} );</script>
+	
 </head>
 
-<body>
-<nav class="navbar navbar-default">
+
+<!--Nav bar settings-->
+<body style="padding-left:-1px;">
+    <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header"><a class="navbar-brand" href="#"> </a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
             <div
@@ -67,25 +54,32 @@ session_start(); //start user session to send data between pages
     </nav>
     <div>
 	<!--Nav bar settings-->
-        <nav class="navbar navbar-default navigation-clean" style="background-color:rgb(72,143,174);min-width:0px;max-width:100%;margin-right:0px;margin-top:-51px;">
+        <nav class="navbar navbar-default navigation-clean" style="background-color:rgb(72,143,174);min-width:0px;max-width:10001px;margin-right:0px;margin-top:-51px;">
             <div class="container">
-              <div class="navbar-header"><a class="navbar-brand" href="/home">Meridian Solutions</a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-			  </div>
+                <div class="navbar-header"><a class="navbar-brand" href="/home">Meridian Solutions</a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
                 <div
                     class="collapse navbar-collapse" id="navcol-1">
                     <ul class="nav navbar-nav navbar-right" style="margin-top:0px;margin-right:-20px;">
-					   <?php if($_SESSION["userType"] == 'admin') { ?><li role="presentation"><a href="/add_item" style="color:rgb(51,51,51);">Add Item</a></li><?php } ?> <!-- if an admin, show add item button -->
+					   <li role="presentation"><a href="/add_item" style="color:rgb(51,51,51);">Add Item</a></li>
                         <li role="presentation"><a href="/edit_table" style="color:rgb(51,51,51);">Edit Table</a></li>
-						<?php if($_SESSION["userType"] == 'admin') { ?><li role="presentation"><a href="/settings" style="color:rgb(51,51,51);">Settings </a></li><?php } ?> <!-- if an admin, show settings button -->
+						<li role="presentation"><a href="/settings" style="color:rgb(51,51,51);">Settings </a></li>
 						<li role="presentation"><a href="/reports" style="color:rgb(51,51,51);">Reports </a></li>
-                        <li role="presentation"><a href="/logout.php" style="color:rgb(51,51,51);">Logout </a></li>							
+                        <li role="presentation"><a href="/logout.php" style="color:rgb(51,51,51);">Logout </a></li>
+                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#"> </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li role="presentation"><a href="#">First Item</a></li>
+                                <li role="presentation"><a href="#">Second Item</a></li>
+                                <li role="presentation"><a href="#">Third Item</a></li>
+								<li role="presentation"><a href="#">Fourth Item</a></li>
+								
+                            </ul>
+                        </li>
                     </ul>
-				</div>
-			</div>
-		</nav>
+            </div>
     </div>
-
-<div class="container">
+    </nav>
+    </div>
+	<div class="container">
 	<div class="panel panel-default panel-table">
               <div class="panel-heading">
                 <div class="row">
@@ -95,20 +89,21 @@ session_start(); //start user session to send data between pages
                 </div>
               </div>
 			  <div class="panel-body">
-			<table id="assets" class="display" cellspacing="0" width="100%">
+			<table id="example" class="display" cellspacing="0" width="100%">
 				<thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Serial Number</th>
-						<th>Brand </th>
-						<th>Model </th>
-						<th>Assigned</th>
-						<th>Location</th>
-						<th>Cost</th>
-						<th>Date Deployed</th>
-						<th>Date Surplused</th>
-						<th>Last Updated</th>
-					</tr> 
+             <th>Name</th>
+            <th>Serial Number</th>
+			<th>Brand </th>
+			<th>Model </th>
+			<th>Assigned</th>
+			<th>Location</th>
+			<th>Cost</th>
+			<th>Date Deployed</th>
+			<th>Date Surplused</th>
+			<th>Last Updated</th>
+			<th>Edit/Delete</th>
+			</tr> 
 				  </thead>
 
                    <tbody>
@@ -133,6 +128,7 @@ session_start(); //start user session to send data between pages
 							<td>'.$item['date_deployed'].'</td>
 							<td>'.$item['date_surplused'].'</td>
 							<td>'.$item['last_updated'].'</td>
+							<td>'.$item['Edit/Delete'].'</td>
 							</tr> 
 							'; 
 						}
@@ -142,8 +138,7 @@ session_start(); //start user session to send data between pages
 			</table>
 			</div>
 			</div>
-  </div>
-
+</div>
 </body>
 	
 	
