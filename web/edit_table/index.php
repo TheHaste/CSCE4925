f23cc8c4afbbc5795e38618f32bffaac899f20dc
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-23 10:55:18 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -31,25 +31,20 @@ session_start(); //start user session to send data between pages
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script> 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/select/1.2.5/css/select.dataTables.min.css"></link>
-<script src="https://cdn.datatables.net/select/1.2.5/js/dataTables.select.min.js"></script>
-<script src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
 
 	<!-- DataTable Javascript Implementation -->
 	<script type="text/javascript">
-		//var editor;
 	$(document).ready(function() {
 		$('#assets').DataTable(
 		 {
-			dom:'Bfrtip',
+			dom: 'Bfrtip',
+			
 			buttons: [
-				'edit','delete'
-			//	{ extend: 'create', editor: myEditor },
-		//{ extend: 'edit',   editor: myEditor },
-		//{ extend: 'remove', editor: myEditor }
-	]
-} );
-	//} );
+				'Edit','Delete'
+					
+				
+			]	
+	} );
 	
 </script>
 
@@ -59,7 +54,7 @@ session_start(); //start user session to send data between pages
 <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header"><a class="navbar-brand" href="#"> </a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
-            <div
+            <div>
                 class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav navbar-right"></ul>
         </div>
