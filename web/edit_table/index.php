c0fce478460c8fea58485fd56446cc9d4cc6931b
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-02-22 22:07:02 -0800

    Update index.php

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -10,7 +10,7 @@ session_start(); //start user session to send data between pages
 <html>
 
 <head>
-    <title>edit_table</title>
+    <title>Meridian Inventory</title>
 	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/Navigation-with-Search1.css">
 	
@@ -26,26 +26,51 @@ session_start(); //start user session to send data between pages
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.bootstrap.min.js"></script>	
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
-	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
-	
-	<script src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
-	<link rel= "stylesheet" href="https://editor.datatables.net/extensions/Editor/css/editor.dataTables.min.css"></link>
-	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script> 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
-	<!-- DataTable Javascript Implementation -->
 	<!-- DataTable Javascript Implementation -->
 	<script type="text/javascript">
 	$(document).ready(function() {
-		$('#example').DataTable();	
-	} );</script>
-
+		$('#assets').DataTable(
+		 {
+			dom: 'Bfrtip',
+			lengthMenu: [
+				[ 10, 25, 50, -1 ],
+				[ '10 rows', '25 rows', '50 rows', 'Show all' ]
+			],
+			buttons: [
+				'pageLength',
+				{extend: 'pdf',
+					text: 'Export to PDF',
+					filename: 'Meridian Inventory',
+					exportOptions: {
+						modifier: {
+							page: 'current'
+						}
+					}
+				},
+				{extend: 'excel',
+					text: 'Export to Excel',
+					filename: 'Meridian Inventory',
+					exportOptions: {
+						modifier: {
+							page: 'current'
+						}
+					}
+				}
+			]
+		});	
+	} );
 	
+</script>
+
 </head>
 
-
-<!--Nav bar settings-->
-<body style="padding-left:-1px;">
-    <nav class="navbar navbar-default">
+<body>
+<nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header"><a class="navbar-brand" href="#"> </a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
             <div
@@ -56,32 +81,25 @@ session_start(); //start user session to send data between pages
     </nav>
     <div>
 	<!--Nav bar settings-->
-        <nav class="navbar navbar-default navigation-clean" style="background-color:rgb(72,143,174);min-width:0px;max-width:10001px;margin-right:0px;margin-top:-51px;">
+        <nav class="navbar navbar-default navigation-clean" style="background-color:rgb(72,143,174);min-width:0px;max-width:100%;margin-right:0px;margin-top:-51px;">
             <div class="container">
-                <div class="navbar-header"><a class="navbar-brand" href="/home">Meridian Solutions</a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
+              <div class="navbar-header"><a class="navbar-brand" href="/home">Meridian Solutions</a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+			  </div>
                 <div
                     class="collapse navbar-collapse" id="navcol-1">
                     <ul class="nav navbar-nav navbar-right" style="margin-top:0px;margin-right:-20px;">
-					   <li role="presentation"><a href="/add_item" style="color:rgb(51,51,51);">Add Item</a></li>
+					   <?php if($_SESSION["userType"] == 'admin') { ?><li role="presentation"><a href="/add_item" style="color:rgb(51,51,51);">Add Item</a></li><?php } ?> <!-- if an admin, show add item button -->
                         <li role="presentation"><a href="/edit_table" style="color:rgb(51,51,51);">Edit Table</a></li>
-						<li role="presentation"><a href="/settings" style="color:rgb(51,51,51);">Settings </a></li>
+						<?php if($_SESSION["userType"] == 'admin') { ?><li role="presentation"><a href="/settings" style="color:rgb(51,51,51);">Settings </a></li><?php } ?> <!-- if an admin, show settings button -->
 						<li role="presentation"><a href="/reports" style="color:rgb(51,51,51);">Reports </a></li>
-                        <li role="presentation"><a href="/logout.php" style="color:rgb(51,51,51);">Logout </a></li>
-                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#"> </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li role="presentation"><a href="#">First Item</a></li>
-                                <li role="presentation"><a href="#">Second Item</a></li>
-                                <li role="presentation"><a href="#">Third Item</a></li>
-								<li role="presentation"><a href="#">Fourth Item</a></li>
-								
-                            </ul>
-                        </li>
+                        <li role="presentation"><a href="/logout.php" style="color:rgb(51,51,51);">Logout </a></li>							
                     </ul>
-            </div>
+				</div>
+			</div>
+		</nav>
     </div>
-    </nav>
-    </div>
-	<div class="container">
+
+<div class="container">
 	<div class="panel panel-default panel-table">
               <div class="panel-heading">
                 <div class="row">
@@ -91,21 +109,20 @@ session_start(); //start user session to send data between pages
                 </div>
               </div>
 			  <div class="panel-body">
-			<table id="example" class="display" cellspacing="0" width="100%">
+			<table id="assets" class="display" cellspacing="0" width="100%">
 				<thead>
                     <tr>
-             <th>Name</th>
-            <th>Serial Number</th>
-			<th>Brand </th>
-			<th>Model </th>
-			<th>Assigned</th>
-			<th>Location</th>
-			<th>Cost</th>
-			<th>Date Deployed</th>
-			<th>Date Surplused</th>
-			<th>Last Updated</th>
-			<th>Edit/Delete</th>
-			</tr> 
+                        <th>Name</th>
+                        <th>Serial Number</th>
+						<th>Brand </th>
+						<th>Model </th>
+						<th>Assigned</th>
+						<th>Location</th>
+						<th>Cost</th>
+						<th>Date Deployed</th>
+						<th>Date Surplused</th>
+						<th>Last Updated</th>
+					</tr> 
 				  </thead>
 
                    <tbody>
@@ -130,7 +147,6 @@ session_start(); //start user session to send data between pages
 							<td>'.$item['date_deployed'].'</td>
 							<td>'.$item['date_surplused'].'</td>
 							<td>'.$item['last_updated'].'</td>
-							<td>'.$item['Edit/Delete'].'</td>
 							</tr> 
 							'; 
 						}
@@ -140,7 +156,8 @@ session_start(); //start user session to send data between pages
 			</table>
 			</div>
 			</div>
-</div>
+  </div>
+
 </body>
 	
 	
