6f4df8085bd12c8446b8c47045616e04a8d99d85
tavonh1 <30598747+tavonh1@users.noreply.github.com>
2018-02-24 16:40:38 -0600

    Update index.php

--- web/add_item/index.php
+++ web/add_item/index.php
@@ -9,20 +9,19 @@
 	
 	if(isset($_POST['Save']))
 	{
-			$item = $_POST['Item Name'];
-			$serial = $_POST['Serial #'];
+			$item = $_POST['Item_Name'];
+			$serial = $_POST['Serial_#'];
 			$location = $_POST['Location'];
 			$model = $_POST['Model'];
 			$brand = $_POST['Brand'];
 			$status = $_POST['Status'];
 			$cost = $_POST['Cost'];
 			
-			$query = "INSERT INTO assets VALUES ('$_POST["a"]', '$_POST["b"]', '$_POST["c"]', '$_POST["d"]', '$_POST["e"]', '$_POST["f"]', '$_POST["123"]')";
-			
+/*			$query = "INSERT INTO assets VALUES ('$_POST['Item_Name']', '$_POST['Serial_#')]', '$_POST['Location']', '$_POST['Model']', '$_POST['Brand']', '$_POST['Status']', '$_POST['Cost']');
 			$result = pg_query($query);
 			echo $item;
 
-	}
+*/	}
 	else
 	{
 			echo "Error adding data to database";
@@ -114,7 +113,7 @@
                     <form id="demo-form2" data-parsley-validate="" class="form-horizontal form-label-left">
 						<!--Create Item Name entry--> 
                       <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12"  style="color:rgb(51,51,51);" name="Item Name"> Item Name <span class="required">*</span>
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12"  style="color:rgb(51,51,51);" name="Item_Name"> Item Name <span class="required">*</span>
                         </label> 					
                         <div class="col-md-6 col-sm-6 col-xs-12">
                           <input type="text" id="Item Name" required="required" class="form-control col-md-7 col-xs-12">
@@ -122,7 +121,7 @@
                       </div>
 						<!--Create Serial # entry-->
                       <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" style="color:rgb(51,51,51);" name="Serial #">Serial # <span class="required">*</span>
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" style="color:rgb(51,51,51);" name="Serial_#">Serial # <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
                           <input type="text" id="Serial #" name="Serial #" required="required" class="form-control col-md-7 col-xs-12">
