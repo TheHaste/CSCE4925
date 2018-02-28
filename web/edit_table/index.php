b34eda78c2d487e4e911414f02b5de01f6c524ee
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-02-27 21:24:16 -0600

    Edit Table Index

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -44,7 +44,9 @@ session_start(); //start user session to send data between pages
  
 $(document).ready(function() {
     var editor = new $.fn.dataTable.Editor( {
-        ajax: '/Editor-PHP-1.7.2/php/assets_config.php',
+        ajax: {"url":'/Editor-PHP-1.7.2/php/assets_config.php',
+				"type": "POST"
+		},
         table: '#assets',
         fields: [ {
                 label: "Name:",
