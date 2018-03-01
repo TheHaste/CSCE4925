e3f21f5c276feb14b5cc06320ecfb561ba96b863
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-02-28 21:05:07 -0600

    Edit Table Index

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -84,7 +84,7 @@ $(document).ready(function() {
 	
 	editor.on( 'initSubmit', function ( ){ //before edit, update timestamp for last edited
 			editor
-				.set('last_updated', "<?php $today = date("Y-m-d"); $today->modify('-1 day'); echo $today->format("Y-m-d");?>")
+				.set('last_updated', "<?php $today = date("Y-m-d"); date_sub($today,date_interval_create_from_date_string("1 day")); echo date_format($today,"Y-m-d");?>")
     } );
  
 	$('#assets').DataTable( {
