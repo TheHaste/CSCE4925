bfbbedeb5df1f4dbb1b569a986342334ea802acd
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-02-28 19:04:56 -0600

    Edit Table Index

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -84,7 +84,8 @@ $(document).ready(function() {
 	
 	editor.on( 'preSubmit', function ( e, o, action ) { //before edit, update timestamp for last edited
         if ( action === 'edit' ) { //edit is selected
-            o.data.last_updated = "<?php echo date("Y-m-d");?>";
+            //o.data.last_updated = "<?php echo date("Y-m-d");?>";
+			o.data.last_updated = new Date('2018-2-1');
         }
 		
     } );
