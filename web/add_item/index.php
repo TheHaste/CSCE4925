75f259a4f43458203b3ec88cc98205a769c78fd7
tavonh1 <30598747+tavonh1@users.noreply.github.com>
2018-03-02 22:58:05 -0600

    Update index.php

--- web/add_item/index.php
+++ web/add_item/index.php
@@ -18,7 +18,7 @@
 			$cost = $_POST['Cost'];
 			
 			/*(name_id, serial_number, location, model, brand, status, cost)*/
-			$query = "INSERT INTO assets VALUES ('$_POST['Item_Name']', '$_POST['Serial_#']', '$_POST['Location']', '$_POST['Model']', '$_POST['Brand']', '$_POST['Status']', '$_POST['Cost']')";
+			$query = "INSERT INTO assets VALUES ('$_POST['Item_Name']', '$_POST['Serial_#']', '$_POST['Location']', '$_POST['Model']', '$_POST['Brand']', '$_POST['Status']', '$_POST['Cost']');";
 			
 			//$result = pg_query($query);
 			
