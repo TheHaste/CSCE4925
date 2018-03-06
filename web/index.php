acad3e0b5dc72a4358cdf95cb8f4a6747aca8ce3
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-05 21:56:23 -0600

    Login Index

--- web/index.php
+++ web/index.php
@@ -111,8 +111,8 @@
         <div class="row row-login">
 			<?php if(isset($err)){ ?><div class="alert alert-danger" role="alert"> <?php echo $err; ?> </div><?php } ?>
 			<div style="height:30px"></div>
-			<div style="text-align:center;width:200px;height:100;"><img src="/assets/img/meridian_logo_large.png"></div>
             <div class="col-10 col-sm-6 col-md-4 offset-1 offset-sm-3 offset-md-4">
+				<img src="/assets/img/meridian_logo_large.png">
 				<div style="height:30px"></div>
 				<h1 class="text-center">Inventory</h1>
 				<div style="height:30px"></div>
