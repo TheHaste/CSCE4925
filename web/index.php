70d7f56b93f8dfd5926f081059a9b681bba7e909
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-05 19:16:28 -0600

    Login Index

--- web/index.php
+++ web/index.php
@@ -1,58 +1,83 @@
 <?php
-//Login Index
+	session_start(); //start user session to send data between pages
+	
+	if($_POST['login button'] == 'LOGIN'){
+		$curl = curl_init();
 
-session_start(); //start user session to send data between pages
+	curl_setopt_array($curl, array(
+	  CURLOPT_PORT => "30443",
+	  CURLOPT_URL => "https://198.61.58.38:30443/v1/authenticate",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 30,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => "POST",
+	  CURLOPT_POSTFIELDS => "username=inventoryadmin&password=3asUSWefup67deSA&secret=%26d%3D_XG%26bngE*-7!*%5EY6r%252W%3DknCy5C_XPfFya_%25gH%408sr5cmQs3%3Dy5jxS39a%5Ebhq%5E7xr*GX6rqR5t9rVRha7WwZNXg3nCvaawYQvk%25%5EKtJtHzmdSZSw_Wptrc!VtJ*F8S9%23nCsFUKD%5EYuyQZabKd%2BRDxeh%3Dt_AJ3%5EAD%26kD59nM-%23MMBCXsvN6SwRVG%23P%23LHsF7Dzq%2BY6fcxpkcMtu%3DQ-P%3FC%3FFWDQZb!9Byy6dW4fuLNXFFzP%3Fg%23y%23J9P4Wv9vR3EK7ES2kq%3FtP%24%24X%2BuF9%2B9%259Fp5PRf787Fjbz9ZX%3DS%242g63Wad%23HhW%26F46ZXPA*C98w%26T%5EnR5w_baXmbVu-%3F7g7vx%2B%5ESutztL4G*bm_RfSkRTK%3F%40HmZ6*YJ4He!-cWE3%3FT48ZrPkCS%40M9Vd_nt%2B%25%26%40G4Mgsu88RJe9F5TfC*XYU%3Fy5dU%24%40CCGFLdgA*yX5FujE%25q6CGZYZ*qu4!6%25EdjaH4f%3DrMp7%25tt_g%2BJabSs%24vAppNgH9kkw5M%5E_WNs%25JN%25GKw3C%2BjNh*_7QP5%2Bf*WVZmNymjk63p%25%23%25UW%23pt2ACuMa8DNF4C%3D6x%23vc%5EVRzY2x%5EEeE!j69QMJ%3F4dU%23%23P%5E%23-kaWUh2Cb6pJ2E7bD_tx%25t%5E%40%5ECdCAastFCYjFSzLZ%258U5%3DNW39ua5k%2Bw9gRw_Pgm96v%23AQq5Ax*ZeR%3F4e3AsB5n%242eKN%26MnjrM%2B%2BwVWUAsx%24yqC6Z4dkNeZc!Bsq%24%25%25%40%5E-E_Tpc8CWuJKdWcv8mP!r%23nLs%24nb%3F7*qf%2B6dZjfL!UtNM2ZPVzVTt%5EpRgxzh%3D9%25%263uyg4kVB%3D69uN_WG*2ZUmx%3FEHh%3Df%26%2BRxt2_HV%5EP7r9wQCXhQUZN6gQzNhPG*7M9%2Buctb9d%40He9J%25VyZpkhAP*%24Z5QqbGNR6V66j%23%3F%2B-XfUzBZDZd%3FuG%40g%3DtSwG%3DatsJjYWJcRZHt%24pK%40fAvcrp56EsjAmkcvS-VH!Q%2BAgMbDvsXMH2qGDSRq%2B2v4Ya7EWCcrn8%24!x%24Kz*t%24xq4H%3DrB-q4%40!%25Z5bC%40K-BCjvK7AwX%26H_Gc7gV",
+	  CURLOPT_SSL_VERIFYHOST => FALSE,
+	  CURLOPT_SSL_VERIFYPEER => FALSE,
+	  CURLOPT_RETURNTRANSFER, TRUE,
+	  CURLOPT_HTTPHEADER => array(
+		"cache-control: no-cache",
+		"content-type: application/x-www-form-urlencoded"
+	  ),
+	));
+	
+	var_dump(json_decode($curl_exec($curl), true);
+	
+	$array['firstName'] = $firstName;
+	$array['lastName'] = $lastName;
+	$array['displayName'] = $displayName
+	$array['username'] = $username
+	$array['memberOf'] = $memberOf
+	
+	echo $firstName;
+	
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
 
-if($_POST['admin'] == 'Admin Button'){ //if admin button is pressed
-	$_SESSION["userType"] = 'admin';
-	header('Location: /home');
-}
+	curl_close($curl);
 
-if($_POST['user'] == 'User Button'){ //if user button is pressed
-	$_SESSION["userType"] = 'user';
-	header('Location: /home/');
-}
-
+	if ($err) {
+	  echo "cURL Error #:" . $err;
+	} 
+	else {
+	  echo $response;
+	}
+		
+	}
+	
+	
 ?>
-
-
-
-
-
 <html>
-
-
-
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
-	 <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Center.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
 <body>
     <div class="container">
-        <div class="row row-login" style="margin-left:-25px;">
-            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
-                <h1 class="text-center">Giganto Asset Management</h1>
-                <div class="well">
-                    <form method= "POST" action="/authenticate.php">
-						<input class="btn btn-success btn-block" type="submit" value="Admin Button" name='admin' style="margin-bottom:21px;margin-top:19px;"></input>
-						<div style="height:5px"></div>
-					    <input class="btn btn-success btn-block" type="submit" value="User Button" name='user' style="margin-bottom:21px;margin-top:19px;"></input>
-					</form>
+        <div class="row row-login">
+            <div class="col-10 col-sm-6 col-md-4 offset-1 offset-sm-3 offset-md-4">
+                <h1 class="text-center">Meridian Business Solutions Inventory</h1>
+                <div class="card">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group"><label>Username</label><input class="form-control" type="text"></div>
+                            <div class="form-group"><label>Password </label><input class="form-control" type="password"></div>
+							<button class="btn btn-success btn-block" type="submit" name="login button" value="LOGIN" >LOGIN</button></form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="/js/jquery-1.12.4.min.js"></script>
-    <script src="/bootstrap/js/bootstrap.min.js"></script>
-    <script id="bs-live-reload" data-sseport="50697" data-lastchange="1516477633766" src="/js/livereload.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
