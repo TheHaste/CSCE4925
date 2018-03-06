d1615fa6bf70fd5260ea2f35ab7ff603c322d504
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-03-06 15:50:54 -0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -76,11 +76,12 @@
 					$_SESSION["userType"] = 'admin';
 					$username = $_POST['username'];
 					$password = $_POST['password'];
+					echo "hello";
 					$login_timeadmin = $_POST['username'];
 					$currentDateTime = new\DateTime();
 					$currentDateTime->setTimezone(new \DateTimeZone('America/Chicago'));
 					$login_timeadmin = $currentDateTime->format('l-j-M-Y H:i:s A');
-					$query = "INSERT INTO logging('user_type','time_stamp') VALUES('$username','$login_timeadmin')";
+					$query = "INSERT INTO logging(user_type,time_stamp) VALUES('$username','$login_timeadmin')";
 					$results = pg_query($conn, $query);
 				}
 				
