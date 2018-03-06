f93f64ba33b49f88902520e3c23e23b13b744ef7
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-03-06 15:52:28 -0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -76,13 +76,13 @@
 					$_SESSION["userType"] = 'admin';
 					$username = $_POST['username'];
 					$password = $_POST['password'];
-					echo "hello";
 					$login_timeadmin = $_POST['username'];
 					$currentDateTime = new\DateTime();
 					$currentDateTime->setTimezone(new \DateTimeZone('America/Chicago'));
 					$login_timeadmin = $currentDateTime->format('l-j-M-Y H:i:s A');
 					$query = "INSERT INTO logging(user_type,time_stamp) VALUES('$username','$login_timeadmin')";
 					$results = pg_query($conn, $query);
+					echo "hello";
 				}
 				
 				if($memberOf == "inventory_user_group"){
