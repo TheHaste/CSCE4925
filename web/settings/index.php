a17d5928406b47317593d7da458c80c9fb8ec3b7
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-03-07 15:25:46 -0800

    Create index.php

---
+++ web/settings/index.php
@@ -0,0 +1,12 @@
+$('#assets').dataTable( {
+  "language": {
+    "lengthMenu": 'Display <select>'+
+      '<option value="10">10</option>'+
+      '<option value="20">20</option>'+
+      '<option value="30">30</option>'+
+      '<option value="40">40</option>'+
+      '<option value="50">50</option>'+
+      '<option value="-1">All</option>'+
+      '</select> records'
+  }
+} );
