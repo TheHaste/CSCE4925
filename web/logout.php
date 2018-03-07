dadfa065b10fce2087a9c1bd0afc92df1b71d753
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-06 20:49:14 -0600

    Logout script

--- web/logout.php
+++ web/logout.php
@@ -1,4 +1,19 @@
 <?php 
+	session_start();
+	date_default_timezone_set('America/Chicago'); //set timezone to CST
+	
+	$username = $_SESSION['username'];
+	
+	//create log
+	include('/app/web/connect.php');
+				
+	$action = "Logout";
+	$log_time = date('M-d-Y H:i:s A');
+					
+	$query = "INSERT INTO logging VALUES ('{$username}', '{$action}', '{$log_time}');";
+				
+	$results = pg_query($conn, $query);
+
 	$_SESSION = array();
 	session_destroy();
 	pg_close($conn);
