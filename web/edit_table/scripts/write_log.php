2a67a080e07e7a3062916526f92b2b66897361b7
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-07 18:26:21 -0600

    Write Log Script

---
+++ web/edit_table/scripts/write_log.php
@@ -0,0 +1,29 @@
+<?php 
+	session_start();
+	include('/app/web/connect.php');
+	
+	$username = $_SESSION["username"]; 
+	$action = $_POST['action'];
+	
+	if($action == 'create'){
+		$action_string
+	}
+	else if($action == 'edit'){
+		$action_string
+	}
+	else if($action == 'remove'){
+		$action_string
+	}
+	else{
+		$empty_action = true;
+	}
+	
+	if($empty_action != true){
+		
+		$log_time = date('M-d-Y H:i:s A'); 
+		
+		$query = "INSERT INTO logging VALUES ('{$username}', '{$action}', '{$log_time}');"; $results = pg_query($conn, $query);
+		
+		pg_close($conn);
+	}
+?>
