9ca8d52f657e7e9cadd53b89e66056566d611de2
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-07 18:50:01 -0600

    Write Log script

--- web/edit_table/scripts/write_log.php
+++ web/edit_table/scripts/write_log.php
@@ -1,6 +1,8 @@
 <?php 
 	session_start();
-	include('/app/web/connect.php');
+	//include('/app/web/connect.php');
+	
+	$conn1 = pg_connect("host=ec2-54-227-243-210.compute-1.amazonaws.com dbname=d3f2mm484o32jn user=tdqtwhcckycshu password=5d86125f0d185bf2918a76dca2adcd104f4a452b71cbcefe831f1d2bd65e98ee");
 	
 	$username = $_SESSION["username"]; 
 	$action = $_POST['action'];
@@ -22,9 +24,11 @@
 		
 		$log_time = date('M-d-Y H:i:s A'); 
 		
-		$query = "INSERT INTO logging VALUES ('{$username}', '{$action}', '{$log_time}');"; $results = pg_query($conn, $query);
+		$query = "INSERT INTO logging VALUES ('{$username}', '{$action_string}', '{$log_time}');"; 
 		
-		pg_close($conn);
+		$results = pg_query($conn1, $query);
+		
+		pg_close($conn1);
 	}
 	else
 		echo "NO LOG WRITTEN";
