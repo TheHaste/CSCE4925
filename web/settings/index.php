0503daa2a9b9b4242aa64fc6bae0bc10d851b497
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-03-07 16:24:09 -0800

    Update index.php

--- web/settings/index.php
+++ web/settings/index.php
@@ -2,6 +2,8 @@
 <html>
 
 <head>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/Navigation-with-Search1.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
@@ -14,32 +16,44 @@
 </head>
 
 <body>
-    <div class="container">
-	<div style="height:10px"></div>
-				<div class="row row-login">
-				<div class="col-10 col-sm-6 col-md-4 offset-1 offset-sm-3 offset-md-4">
-				<div style="text-center;"><img src="/assets/img/login_logo.png"> </div>
-					<div style="height:40px"></div>
-					<div class="card">
-						<div class="card-body">
-							<form method= 'POST'>
-								<label>Username</label> <input class="form-control" type="text" name="username" value="">
-								<div style="height:10px"></div>
-								<label>Password </label> <input class="form-control" type="password" name="password" value="">
-								<div style="height:20px"></div>
-								<button class="btn btn-success btn-block" type="submit" name="login_button">LOGIN</button>
-								</form>
-						</div>
-					</div> 
-				</div> 
-			</div> 
-	</div> 
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-</body>
+<nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header"><a class="navbar-brand" href="#"> </a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav navbar-right"></ul>
+        </div>
+        </div>
+    </nav>
+    <div>
+	<!--Nav bar settings-->
+        <nav class="navbar navbar-default navigation-clean" style="background-color:rgb(72,143,174);min-width:0px;max-width:100%;margin-right:0px;margin-top:-51px;">
+            <div class="container">
+              <div class="navbar-header"><a class="navbar-brand" href="/home">Meridian Solutions</a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+			  </div>
+                <div
+                    class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav navbar-right" style="margin-top:0px;margin-right:-20px;">
+					   
+                        <?php if($_SESSION["userType"] == 'admin') { ?><li role="presentation"><a href="/edit_table" style="color:rgb(51,51,51);">Edit Table</a></li> <?php } ?>  <!-- if an admin, show settings button -->
+						<?php if($_SESSION["userType"] == 'admin') { ?><li role="presentation"><a href="/settings" style="color:rgb(51,51,51);">Settings </a></li> <?php } ?> <!-- if an admin, show settings button -->
+						<li role="presentation"><a href="/reports" style="color:rgb(51,51,51);">Reports </a></li>
+                        <li role="presentation"><a href="/logout.php" style="color:rgb(51,51,51);">Logout </a></li>							
+                    </ul>
+				</div>
+			</div>
+		</nav>
+    </div>
 
-</html>
-
+<div class="container">
+	<div class="panel panel-default panel-table">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col col-xs-6">
+                    <h3 class="panel-title">Meridian Inventory</h3>
+                  </div>
+                </div>
+              </div>
 
 
 
