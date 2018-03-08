163766ada401a391db91caad96c3c1a76e3f0967
RB34 <35539699+reggiebarnes87@users.noreply.github.com>
2018-03-07 16:16:21 -0800

    Update index.php

--- web/settings/index.php
+++ web/settings/index.php
@@ -49,15 +49,4 @@
 
 
 
-/*$('#assets').dataTable( {
-  "language": {
-    "lengthMenu": 'Display <select>'+
-      '<option value="10">10</option>'+
-      '<option value="20">20</option>'+
-      '<option value="30">30</option>'+
-      '<option value="40">40</option>'+
-      '<option value="50">50</option>'+
-      '<option value="-1">All</option>'+
-      '</select> records'
-  }
-} );*/
+
