17a64551005cb334c2958a82cbb73f9851a0e648
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-13 19:13:56 -0500

    Reporting Index

--- web/reports/index.php
+++ web/reports/index.php
@@ -11,8 +11,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meridian Inventory</title>
 	
-	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-
+	<!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script> -->
+	
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 	
 	<!-- DataTable Extensions -->
