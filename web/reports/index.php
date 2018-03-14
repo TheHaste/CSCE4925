230ec6df333df15e52ccd3ce3c6ffa4948779745
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-13 19:25:48 -0500

    Reporting Index

--- web/reports/index.php
+++ web/reports/index.php
@@ -1,22 +1,24 @@
-<?php 
-	
-	require('/app/web/connect.php');
-	session_start(); //start user session to send data between pages
+<?php
+//Home Index
+
+require('/app/web/connect.php');
+
+session_start(); //start user session to send data between pages
 
 ?>
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meridian Inventory</title>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/Navigation-with-Search1.css">
 	
 	<!-- DataTable Extensions -->
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"></link> 
 	<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script> 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
-	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 	<!-- DataTable Buttons Extensions -->
 	<link rel="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css"></link>
@@ -27,13 +29,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script> 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-	
-	<!-- Page Assets -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-	
-	
+
 	<!-- DataTable Javascript Implementation -->
 	<script type="text/javascript">
 	$(document).ready(function() {
@@ -70,66 +66,100 @@
 	
 
 </script>
-	
+
 </head>
 
 <body>
-    <div>
+<nav class="navbar navbar-default">
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-					<table id="assets" class="display" cellspacing="0" width="100%">
-						<thead>
-							<tr>
-								<th>Name</th>
-								<th>Serial Number</th>
-								<th>Brand </th>
-								<th>Model </th>
-								<th>Assigned</th>
-								<th>Location</th>
-								<th>Cost</th>
-								<th>Date Deployed</th>
-								<th>Date Surplused</th>
-								<th>Last Updated</th>
-							</tr> 
-						  </thead>
-
-						  <tbody>
-							
-							<?php
-								//fill table
-								$query = "SELECT * FROM assets;";
-								$item = array(); //array for assets
-
-								$rs = pg_query($conn, $query); //run query
-
-								while ($item = pg_fetch_assoc($rs)) //fetch and fill array
-								{
-									//$item[] = $line;
-									echo '
-									<tr>
-									<td>'.$item['name_id'].'</td>
-									<td>'.$item['serial_number'].'</td>
-									<td>'.$item['brand'].'</td>
-									<td>'.$item['model'].'</td>
-									<td>'.$item['assigned'].'</td>
-									<td>'.$item['location'].'</td>
-									<td>'.$item['cost'].'</td>
-									<td>'.$item['date_deployed'].'</td>
-									<td>'.$item['date_surplused'].'</td>
-									<td>'.$item['last_updated'].'</td>
-									</tr> 
-									'; 
-								}
-							?>
-							 
-						   </tbody>
-					</table>
-				</div>
-            </div>
+            <div class="navbar-header"><a class="navbar-brand" href="#"> </a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav navbar-right"></ul>
         </div>
-    </div>
+        </div>
+    </nav>
     <div>
+	<!--Nav bar settings-->
+        <nav class="navbar navbar-default navigation-clean" style="background-color:rgb(72,143,174);min-width:0px;max-width:100%;margin-right:0px;margin-top:-51px;">
+            <div class="container">
+              <div class="navbar-header"><a class="navbar-brand" href="/home"><img src="/assets/img/big_logo_tiny.png"></a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+			  </div>
+                <div
+                    class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav navbar-right" style="margin-top:0px;margin-right:-20px;">
+					   
+                        <?php if($_SESSION["userType"] == 'admin') { ?><li role="presentation"><a href="/edit_table" style="color:rgb(51,51,51);">Edit Table</a></li> <?php } ?>  <!-- if an admin, show settings button -->
+						<?php if($_SESSION["userType"] == 'admin') { ?><li role="presentation"><a href="/settings" style="color:rgb(51,51,51);">Settings </a></li> <?php } ?> <!-- if an admin, show settings button -->
+						<li role="presentation"><a href="/reports" style="color:rgb(51,51,51);">Reports </a></li>
+                        <li role="presentation"><a href="/logout.php" style="color:rgb(51,51,51);">Logout </a></li>							
+                    </ul>
+				</div>
+			</div>
+		</nav>
+    </div>
+
+<div class="container">
+	<div class="panel panel-default panel-table">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col col-xs-6">
+                    <h3 class="panel-title">Meridian Inventory</h3>
+                  </div>
+                </div>
+              </div>
+			  <div class="panel-body">
+			<table id="assets" class="display" cellspacing="0" width="100%">
+				<thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Serial Number</th>
+						<th>Brand </th>
+						<th>Model </th>
+						<th>Assigned</th>
+						<th>Location</th>
+						<th>Cost</th>
+						<th>Date Deployed</th>
+						<th>Date Surplused</th>
+						<th>Last Updated</th>
+					</tr> 
+				  </thead>
+
+                   <tbody>
+					
+					<?php
+						//fill table
+						$query = "SELECT * FROM assets;";
+						$item = array(); //array for assets
+
+						$rs = pg_query($conn, $query); //run query
+
+						while ($item = pg_fetch_assoc($rs)) //fetch and fill array
+						{
+							//$item[] = $line;
+							echo '
+							<tr>
+							<td>'.$item['name_id'].'</td>
+							<td>'.$item['serial_number'].'</td>
+							<td>'.$item['brand'].'</td>
+							<td>'.$item['model'].'</td>
+							<td>'.$item['assigned'].'</td>
+							<td>'.$item['location'].'</td>
+							<td>'.$item['cost'].'</td>
+							<td>'.$item['date_deployed'].'</td>
+							<td>'.$item['date_surplused'].'</td>
+							<td>'.$item['last_updated'].'</td>
+							</tr> 
+							'; 
+						}
+					?>
+					 
+                   </tbody>
+			</table>
+			</div>
+			</div>
+  </div>
+  <div>
         <div class="container">
             <div class="col-md-12">
                 <p class="help-block">Choose your report type and select fields to filter your report. When filtering one field with multiple criteria, separate with a comma.</p>
@@ -145,7 +175,7 @@
             </div>
         </div>
     </div>
-    <div>
+	<div>
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
@@ -182,8 +212,10 @@
         </div>
         <div style="height:50px;"></div>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+
 </body>
+	
+	
 
 </html>
