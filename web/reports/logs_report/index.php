0f1cd87bfef701a875c2614e9651c3cf7f5ec540
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-19 19:54:43 -0500

    Logs Report Index

--- web/reports/logs_report/index.php
+++ web/reports/logs_report/index.php
@@ -66,7 +66,8 @@
 			if((!(empty($_SESSION['data'][3]))) || (!(empty($_SESSION['data'][4])))){ //atleast another column has data
 				$old_time1 = $_SESSION['data'][2];
 				$new_date1 = date('M-d-Y', strtotime($old_time1));
-				$SQL_where .= "log_time LIKE '%{$new_date1}%' OR "; //append with comma and move to next column
+				//$SQL_where .= "log_time LIKE '%{$new_date1}%' OR "; //append with comma and move to next column
+				$SQL_where .= "log_time >= '%{$new_date1}%' OR "; //append with comma and move to next column
 				return checkFour($SQL_where);
 			}
 			else{ //no other data for query
@@ -88,7 +89,8 @@
 		if((!(empty($_SESSION['data'][3])))){ //last column has data
 			$old_time2 = $_SESSION['data'][3];
 			$new_date2 = date('M-d-Y', strtotime($old_time2));
-			$SQL_where .= "log_time LIKE '%{$new_date2}%'"; //append with comma and move to next column
+			//$SQL_where .= "log_time LIKE '%{$new_date2}%'"; //append with comma and move to next column
+			$SQL_where .= "log_time <= '%{$new_date2}%'"; //append with comma and move to next column
 		}
 		
 		return $SQL_where;
