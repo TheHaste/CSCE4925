6e5efbf1eda623464e58ad20d9293b1ef681bdab
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-22 18:32:32 -0500

    Settings Index

--- web/settings/index.php
+++ web/settings/index.php
@@ -30,14 +30,14 @@
 		div.innerHTML =
 			'<label>Type</label><input type="text" style="margin-left:10px;"> <select style="margin-left:5px; height:27px; width:55px; " name="percent"> <option value="10">10%</option> <option value="20">20%</option> <option value="30">30%</option> <option value="40">40%</option> <option value="50">50%</option> <option value="60">60%</option> <option value="70">70%</option> <option value="80">80%</option> <option value="90">90%</option> </select> <button class="btn btn-danger" type="button" style="padding:0px; width:25px; vertical-align: middle; margin-left:15px;" onclick="deleteNotification(this)">x</button>';
 		
-		document.getElementsByID('notifications-block').appendChild('div');
+		$('#notifications-block').appendChild('div'); 
 	}
 
 	</script>
 	
 	<script type="text/javascript"> 
 	function deleteNotification(input){
-		document.getElementsByID('notifications-block').removeChild(input.parentNode); 
+		$('#notifications-block').removeChild(input.parentNode); 
 	}
 
 </script>
