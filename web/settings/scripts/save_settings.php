31db94e7e1642105c458304c14d99725343812cb
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-22 20:53:11 -0500

    Save Settings Script

---
+++ web/settings/scripts/save_settings.php
@@ -0,0 +1,46 @@
+<?php 
+	session_start();
+	
+	$_SESSION['settings'] = [];
+	
+	$types = $_POST['types'];
+	$thresholds = $_POST['thresholds'];
+	
+	$settings_array = array($types, $thresholds);
+	
+	$_SESSION['settings'] = $settings_array;
+	
+	//delete old XML
+	delete('/settings/settings_config.xml');
+	
+	//build new XML
+	$xml = new DomDocument('1.0', "UTF-8");
+	$xml->load('settings_config.xml');
+	
+	$rootTag = $xml->getElementByTagName("settings")->$item(0);
+	
+	for($i=0; $i<count($types); i++){
+		if($types[$i] == ""){ //empty asset type
+		}
+		else{
+			//store values for objects
+			$notification=$xml->createElement("notification");
+			$type=$xml->createElement("type", $types[$i]);
+			$threshold=$xml->createElement("threshold", $thresholds[$i]);
+			
+			//append attributes
+			$notification->appendChild($type);
+			$notification->appendChild($threshold);
+			
+			//append child object to root
+			$rootTag->appendChild($notification);
+			
+		}
+	}
+		
+	//save new XML
+	$xml->formatoutput = true;
+		
+	$xml->save("settings_config.xml");
+	
+?>
