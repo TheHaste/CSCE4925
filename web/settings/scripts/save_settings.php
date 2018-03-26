d13b2153a8a345b80dd22211e0dacc47852d92a6
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-26 18:03:47 -0500

    Save Settings Script

--- web/settings/scripts/save_settings.php
+++ web/settings/scripts/save_settings.php
@@ -49,6 +49,9 @@
 	$total_notifications = sizeof($types);
 	$total_db_notifications = sizeof($db_types);
 	
+	array_push($_SESSION['settings'], $db_types);
+	array_push($_SESSION['settings'], $db_thresholds);
+	
 	//check for settings to add
 	for($i=0; $i<$total_notifications; $i++){
 		$add = true;
