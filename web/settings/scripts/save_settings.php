ab166e86727f2ba6ef4b864a1f4dcea49220a700
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-27 11:29:34 -0500

    Save Settings Index

--- web/settings/scripts/save_settings.php
+++ web/settings/scripts/save_settings.php
@@ -102,7 +102,7 @@
 	
 	$index1=0;
 	//insert new settings into table
-	for($i=0; $i<sizeof($types_to_delete); $i++){
+	for($i=0; $i<sizeof($types_to_save); $i++){
 		$addSQL = "INSERT INTO notification_settings (type, threshold) VALUES ('{$types_to_save[$index1]}','{$thresholds_to_save[$index1]}');";
 		$add_query = pg_query($conn, $addSQL); //run query
 		$index1++;
