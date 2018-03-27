d63b78f801ebacaad088bba84010b45c53fe9446
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-27 12:15:09 -0500

    Save Settings Index

--- web/settings/index.php
+++ web/settings/index.php
@@ -138,6 +138,7 @@
 											echo '<label>Type</label><input type="text" value="'.$_SESSION['settings'][0][$index].'"style="margin-left:10px;"> <select style="margin-left:5px; height:27px; width:55px; "> <option value="10">10%</option> <option value="20">20%</option> <option value="30">30%</option> <option value="40">40%</option> <option value="50">50%</option> <option value="60">60%</option> <option value="70">70%</option> <option value="80">80%</option> <option selected value="90">90%</option> </select> <button class="btn btn-danger" type="button" style="padding:0px; width:25px; vertical-align: middle; margin-left:15px;" onclick="deleteNotification(this)">x</button>';
 										}
 									}
+									echo '</br>';
 									$index++;
 								}
 							?>
