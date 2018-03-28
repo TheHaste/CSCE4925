1be527fec31825d52dd10df652d236fb0b39c5c7
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-27 22:19:56 -0500

    Home Index

--- web/home/index.php
+++ web/home/index.php
@@ -1,59 +1,62 @@
 <?php
 //Home Index
 
-require('/app/web/connect.php');
+	require('/app/web/connect.php');
 
-session_start(); //start user session to send data between pages
+	session_start(); //start user session to send data between pages
 
-$types = [];
-$thresholds = [];
-$alerts = [];
-
-//retrieve notification_settings
-	$query = "SELECT * FROM notification_settings;";
+	//retrieve monitoring_settings
+	$query = "SELECT * FROM monitoring_settings;";
 	$item = array(); //array for assets
 	$rs = pg_query($conn, $query); //run query
 
 	while ($item = pg_fetch_assoc($rs)){ //fetch and fill array
-		array_push($types, $item['type']);
-		array_push($thresholds, $item['threshold']);
+		array_push($monitoring_settings, $item['status']);
 	}
 	
-	$num_of_notifications = sizeof($types);
-	$index = 0;
-	//check alerts/notifications
-	for($i=0; $i<$num_of_notifications; $i++){
-		//query for count of items with matching type
-		$query = "SELECT * FROM assets WHERE asset_type='{$types[$index]}';";
+	if($monitoring_settings[1] == "ON"){
+	
+		$types = [];
+		$thresholds = [];
+		$alerts = [];
+
+		//retrieve notification_settings
+		$query = "SELECT * FROM notification_settings;";
+		$item = array(); //array for assets
 		$rs = pg_query($conn, $query); //run query
 
-		$type_total = floatval(pg_num_rows($rs)); //fetch result
-	
-		//query for count of items with matching type and assigned is empty
-		$query = "SELECT * FROM assets WHERE asset_type='{$types[$index]}' AND assigned='';";
-		$rs = pg_query($conn, $query); //run query
-
-		$available_total = floatval(pg_num_rows($rs)); //fetch result
-	
-		//calculate if threshold is reached
-		$calculation = 100 * floatval($available_total / $type_total);
-		
-		//if threshold reached
-		if(floatval($calculation) >= floatval($thresholds[$index]) ){
-			//append type to the alerts array
-			array_push($alerts, $types[$index]);
+		while ($item = pg_fetch_assoc($rs)){ //fetch and fill array
+			array_push($types, $item['type']);
+			array_push($thresholds, $item['threshold']);
 		}
-		$index++;
-	}	
-	
-	echo "calculation: {$calculation}";
-	echo "</br>";
-	echo "type_total: {$type_total}";
-	echo "</br>";
-	echo "available_total: {$available_total}";
-	echo "</br>";
-	
-	echo json_encode($alerts);
+		
+		$num_of_notifications = sizeof($types);
+		$index = 0;
+		//check alerts/notifications
+		for($i=0; $i<$num_of_notifications; $i++){
+			//query for count of items with matching type
+			$query = "SELECT * FROM assets WHERE asset_type='{$types[$index]}';";
+			$rs = pg_query($conn, $query); //run query
+
+			$type_total = floatval(pg_num_rows($rs)); //fetch result
+		
+			//query for count of items with matching type and assigned is empty
+			$query = "SELECT * FROM assets WHERE asset_type='{$types[$index]}' AND assigned='';";
+			$rs = pg_query($conn, $query); //run query
+
+			$available_total = floatval(pg_num_rows($rs)); //fetch result
+		
+			//calculate if threshold is reached
+			$calculation = 100 * floatval($available_total / $type_total);
+			
+			//if threshold reached
+			if(floatval($calculation) >= floatval($thresholds[$index]) ){
+				//append type to the alerts array
+				array_push($alerts, $types[$index]);
+			}
+			$index++;
+		}	
+	}
 
 ?>
 <html>
