b344b8c77521b1c9538169fc2fdb7d945745ddd1
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-27 22:39:16 -0500

    Write Log Script

--- web/edit_table/scripts/write_log.php
+++ web/edit_table/scripts/write_log.php
@@ -4,7 +4,7 @@
 	
 	$conn1 = pg_connect("host=ec2-54-227-243-210.compute-1.amazonaws.com dbname=d3f2mm484o32jn user=tdqtwhcckycshu password=5d86125f0d185bf2918a76dca2adcd104f4a452b71cbcefe831f1d2bd65e98ee");
 	
-	//populate settings SESSION
+	//populate settings
 	$monitoring_settings = [];
 		
 	//retrieve monitoring_settings
