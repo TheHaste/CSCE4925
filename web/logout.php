c8abde33b25766ca3e795c25dba574d8b201c00a
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-27 20:24:29 -0500

    Logout Script

--- web/logout.php
+++ web/logout.php
@@ -6,14 +6,31 @@
 	
 	//create log
 	include('/app/web/connect.php');
-				
-	$action = "Logout";
-	$log_time = date('M-d-Y H:i:s A');
-					
-	$query = "INSERT INTO logging VALUES ('{$username}', '{$action}', '{$log_time}');";
-				
-	$results = pg_query($conn, $query);
+	
+	//populate settings SESSION
+	$monitoring_settings = [];
+		
+	//retrieve monitoring_settings
+	$query = "SELECT * FROM monitoring_settings;";
+	$item = array(); //array for assets
+	$rs = pg_query($conn, $query); //run query
 
+	while ($item = pg_fetch_assoc($rs)){ //fetch and fill array
+		array_push($monitoring_settings, $item['status']);
+	}
+	
+	$_SESSION['settings'] = $settings; //save array of data to session
+				
+	//if logs are turned on, capture log
+	if($monitoring_settings[0] == "ON"){
+		$action = "Logout";
+		$log_time = date('M-d-Y H:i:s A');
+						
+		$query = "INSERT INTO logging VALUES ('{$username}', '{$action}', '{$log_time}');";
+					
+		$results = pg_query($conn, $query);
+	}
+	
 	$_SESSION = array();
 	session_destroy();
 	pg_close($conn);
