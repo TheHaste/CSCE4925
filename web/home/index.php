e7339f63a986e3fb1f9b109a89c3189190733691
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-27 21:29:05 -0500

    Home Index

--- web/home/index.php
+++ web/home/index.php
@@ -119,15 +119,15 @@ $alerts = [];
 		});
 
 		
-		$.getJSON('/home/scripts/get_alerts.php', function(data) {
-                var total_alerts = data.length;
-				var message = "Alert: Threshold reached for ";
-				for(i=0; i<total_alerts; i++){
-					toastr.options.timeOut = 0;
-					toastr.options.closeButton = true;
-					toastr.error(message.concat(data[i]));
-				}
-        });		
+		var data = $.getJSON('/home/scripts/get_alerts.php', function() {});		
+		
+		var total_alerts = data.length;
+		var message = "Alert: Threshold reached for ";
+		for(i=0; i<total_alerts; i++){
+			toastr.options.timeOut = 0;
+			toastr.options.closeButton = true;
+			toastr.error(message.concat(data[i]));
+		}
 		
 	} );
 	
