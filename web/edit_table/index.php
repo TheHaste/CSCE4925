8d5f86dac4b4001f1e15c26bccce786962ba1a0e
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-03-27 23:11:18 -0500

    Edit Table Index

--- web/edit_table/index.php
+++ web/edit_table/index.php
@@ -45,6 +45,7 @@ $_SESSION['settings'] = $settings; //save array of data to session
     <title>Meridian Inventory</title>
 	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/Navigation-with-Search1.css">
+	<link rel="stylesheet" href="assets/css/table_width.css">
 
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"></link> 
@@ -179,15 +180,7 @@ $(document).ready(function() {
 </head>
 
 <body>
-<nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header"><a class="navbar-brand" href="#"> </a><button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></div>
-            <div
-                class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav navbar-right"></ul>
-        </div>
-        </div>
-    </nav>
+<div style="height:50px"></div>
     <div>
 	<!--Nav bar settings-->
         <nav class="navbar navbar-default navigation-clean" style="background-color:rgb(72,143,174);min-width:0px;max-width:100%;margin-right:0px;margin-top:-51px;">
