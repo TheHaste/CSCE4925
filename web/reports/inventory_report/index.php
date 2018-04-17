eb6aba0b36b8f2a62f9c98517ca26f40bd2b78a4
TheHaste <35510892+TheHaste@users.noreply.github.com>
2018-04-17 18:47:11 -0500

    Inventory Report Index

--- web/reports/inventory_report/index.php
+++ web/reports/inventory_report/index.php
@@ -576,7 +576,8 @@
   <div>
         <div class="container">
             <div class="col-md-12" style="height:40px;">
-                <p class="help-block">Choose your report type and select fields to filter your report. When filtering one field with multiple criteria, separate with a comma.</p>
+                <p class="help-block">Choose your report type and select fields to filter your report. When filtering one field with multiple criteria, separate with a comma.</p></br>
+				<p class="help-block">For Example: John Smith,Bob,Jane Doe.</p>
 			</div>
 		<form method="post" id="report">
             <div class="col-md-12" style="height:40px;">
